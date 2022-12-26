4025863976d3109c762aba143ef4e89290728ff4
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-12-26 21:43:31 +0100

    validation is right

--- src/CredentialsValidation.js
+++ src/CredentialsValidation.js
@@ -3,7 +3,7 @@ import database from "./database.js";
 async function CredentialsValidation(email, password){
     const email_exists = await database.raw(`select * from users where email='${email}'`);
     const email_validation = email.length > 5 && email.length < 20 && email_exists.length == 0;
-    const password_validation = /^(?=.[0-9])(?=.[!:?.])[a-z0-9!!:?.]{5,20}$/;
+    const password_validation = /^(?=.*[0-9])(?=.*[a-z])(?=.*[!:?.])[a-z0-9!:?.]{5,20}$/;
 
     if(!email_validation){
         throw new Error("Email is not valid!");
